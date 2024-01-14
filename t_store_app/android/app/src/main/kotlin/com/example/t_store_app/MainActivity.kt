f38699c648ece9574d38be8fdccb2f096891e86a
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2024-01-14 08:24:56 +0700

    Re-Created E-Commerce App with new Flutter SDK Version

--- t_store_app/android/app/src/main/kotlin/com/example/t_store_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.t_store_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ t_store_app3/android/app/src/main/kotlin/com/example/t_store_app3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.t_store_app3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
