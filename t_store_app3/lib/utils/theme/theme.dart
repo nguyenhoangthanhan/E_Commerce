675865de826961a316c93b2c6e98d7f992991258
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2024-01-14 10:37:46 +0700

    Created AppBar Theme, elevated button theme, text theme

---
+++ t_store_app3/lib/utils/theme/theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:t_store_app3/utils/theme/custom_themes/text_theme.dart';
+
+class TAppTheme{
+  TAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: "Poppins",
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: TTextTheme.lightTextTheme,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+
+    )
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: "Poppins",
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: TTextTheme.darkTextTheme,
+  );
+}
