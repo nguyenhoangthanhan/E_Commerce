184ab4b98701125ad5db04503ff8847735d68400
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2024-01-19 23:58:19 +0700

    Add facebook, google icons; Add text_strings.dart contains our project strings

--- t_store_app3/lib/utils/constants/image_strings.dart
+++ t_store_app3/lib/utils/constants/image_strings.dart
@@ -7,6 +7,6 @@ class TImages{
   static const String lightAppLogo = "assets/logos/supermaket_white.png";
 
   // -- App Logos
-  static const String darkAppLogo = "assets/logos/supermaket_black.png";
-  static const String lightAppLogo = "assets/logos/supermaket_white.png";
+  static const String google = "assets/logos/supermaket_black.png";
+  static const String facebook = "assets/logos/supermaket_white.png";
 }
