a7291a79a4cb688b15f8dcd0c119a0d8831854d5
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2024-01-21 21:02:41 +0700

    Completed device_utility.dart

--- t_store_app3/lib/utils/devices/device_utility.dart
+++ t_store_app3/lib/utils/devices/device_utility.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class TDeviceUtils {
   static void hideKeyboard(BuildContext context) {
@@ -68,5 +71,45 @@ class TDeviceUtils {
         defaultTargetPlatform == TargetPlatform.iOS;
   }
 
+  static void vibrate(Duration duration){
+    HapticFeedback.vibrate();
+    Future.delayed(duration, () => HapticFeedback.vibrate());
+  }
 
+  static Future<void> setPreferredOrientations(List<DeviceOrientation> orientations) async {
+    await SystemChrome.setPreferredOrientations(orientations);
+  }
+
+  static void hideStatusBar(){
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+  }
+
+  static void showStatusBar(){
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverlay.values);
+  }
+
+  static Future<bool> hasInternetConnection() async {
+    try{
+      final result = await InternetAddress.lookup('example.com');
+      return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+    } on SocketException catch(_){
+      return false;
+    }
+  }
+
+  static bool isIOS(){
+    return Platform.isIOS;
+  }
+
+  static bool isAndroid(){
+    return Platform.isAndroid;
+  }
+
+  static void launchUri(String url) async{
+    if (await canLaunchUrlString(url)){
+      await launchUrlString(url);
+    }else{
+      throw 'Could not launch $url';
+    }
+  }
 }
