71285203e944a6a62fbc7ccf783f3ae17bfe57de
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2024-01-22 23:17:08 +0700

    Created pricing_calculator.dart

---
+++ t_store_app3/lib/utils/helpers/pricing_calculator.dart
@@ -0,0 +1,13 @@
+class TPricingCalculator{
+
+  // -- Calculate Price based on tax and shipping
+  static double calculateTotalPrice(double productPrice, String location){
+    double taxRate = getTaxRateForLocation(location);
+    double taxAmount = productPrice * taxRate;
+
+    double shippingCost = getShippingCost(location);
+
+    double totalPrice = productPrice + taxAmount +shippingCost;
+    return totalPrice;
+  }
+}
