5efa93d82b4328b860ce482f83b5c275853f0e56
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2024-01-23 23:54:48 +0700

    Completed pricing_calculator.dart

--- t_store_app3/lib/utils/helpers/pricing_calculator.dart
+++ t_store_app3/lib/utils/helpers/pricing_calculator.dart
@@ -10,4 +10,34 @@ class TPricingCalculator{
     double totalPrice = productPrice + taxAmount +shippingCost;
     return totalPrice;
   }
+
+  /// -- Calculate shipping cost
+  static String calculateShippingCost(double productPrice, String location){
+    double shippingCost = getShippingCost(location);
+    return shippingCost.toStringAsFixed(2);
+  }
+
+  /// -- Calculate tax
+  static String calculateTax(double productPrice, String location){
+    double taxRate = getTaxRateForLocation(location);
+    double taxAmount = productPrice * taxRate;
+    return taxAmount.toStringAsFixed(2);
+  }
+
+  static double getTaxRateForLocation(String location){
+    // Lookup the tax rate for the given location from a tax rate database or API
+    // Return the appropriate tax rate.
+    return 0.10;
+  }
+
+  static double getShippingCost(String location){
+    // Look up the shipping cost for the given location using a shipping rate API.
+    // Calculate the shipping cost based on various factors like distance, weight, etc.
+    return 5.0;
+  }
+
+  /// -- Sum all cart value and return total amount
+  // static double calculateCartTotal(CartModel cart){
+  //   return cart.items.map((e) => e.price).fold(0, (previousPrice, currentPrice) => previousPrice + (currentPrice ?? 0));
+  // }
 }
