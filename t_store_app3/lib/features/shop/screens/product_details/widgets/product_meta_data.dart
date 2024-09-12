d11e2b58b2e18404e37c6c8e47be828661a1b8da
NHTA1999 <skyannguyenhoangthanh@gmail.com>
2024-09-12 21:20:53 +0700

    Adding brand icons and text

--- t_store_app3/lib/features/shop/screens/product_details/widgets/product_meta_data.dart
+++ t_store_app3/lib/features/shop/screens/product_details/widgets/product_meta_data.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:t_store_app3/common/widgets/containers/rounded_container.dart';
+import 'package:t_store_app3/common/widgets/images/t_circular_image.dart';
 import 'package:t_store_app3/common/widgets/products/product_cards/product_card_vertical.dart';
+import 'package:t_store_app3/common/widgets/texts/t_brand_title_text_with_verified_icon.dart';
 import 'package:t_store_app3/utils/constants/colors.dart';
+import 'package:t_store_app3/utils/constants/enums.dart';
+import 'package:t_store_app3/utils/constants/image_strings.dart';
 import 'package:t_store_app3/utils/constants/sizes.dart';
 
 import '../../../../../common/widgets/texts/product_title_text.dart';
@@ -16,11 +20,9 @@ class TProductMetaData extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-
         /// -- Price & Sale Price
         Row(
           children: [
-
             /// Sale Tag
             TRoundedContainer(
               radius: TSizes.sm,
@@ -29,8 +31,7 @@ class TProductMetaData extends StatelessWidget {
                   horizontal: TSizes.sm, vertical: TSizes.xs),
               child: Text(
                 '25%',
-                style: Theme
-                    .of(context)
+                style: Theme.of(context)
                     .textTheme
                     .labelLarge!
                     .apply(color: TColors.black),
@@ -41,12 +42,9 @@ class TProductMetaData extends StatelessWidget {
             /// Price
             Text(
               '\$250',
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .titleSmall!
-                  .apply(decoration: TextDecoration.lineThrough,
-              ),
+              style: Theme.of(context).textTheme.titleSmall!.apply(
+                    decoration: TextDecoration.lineThrough,
+                  ),
             ),
             const SizedBox(width: TSizes.spaceBtwItems),
             TProductPriceText(
@@ -70,14 +68,27 @@ class TProductMetaData extends StatelessWidget {
               title: 'Status',
             ),
             const SizedBox(height: TSizes.spaceBtwItems),
-            Text('In Stock', style: Theme.of(context).textTheme.titleMedium,),
+            Text(
+              'In Stock',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
           ],
         ),
         const SizedBox(height: TSizes.spaceBtwItems / 1.5),
 
         /// -- Brand
-
-
+        Row(
+          children: [
+            TCircularImage(
+              image: TImages.animalIcon,
+              width: 32,
+              height: 32,
+              overlayColor: darkMode ? TColors.white : TColors.black,
+            ),
+            const TBrandTitleWithVerifiedIcon(
+                title: 'Nike', brandTextSize: TextSizes.medium),
+          ],
+        )
       ],
     );
   }
